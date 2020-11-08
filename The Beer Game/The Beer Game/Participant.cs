e8e45726eb775003660b0722d4344e3a2eb224dc
unzelfunzel <unzel@byom.de>
2020-11-08 19:12:30 +0100

    update

--- The Beer Game/The Beer Game/Participant.cs
+++ The Beer Game/The Beer Game/Participant.cs
@@ -6,12 +6,17 @@ namespace The_Beer_Game
 {
     class Participant
     {
-        Participant()
-        {
-        }
-
+        // create variables for class:
         int Inventory = 0;
         int Bank = 0;
+        
+        // class constructor:
+        Participant(int inv, int money)
+        {
+            // set inventory and bank to given values at class initialization:
+            Inventory = inv;
+            Bank = money;
+        }
         
     }
 
