9e9df8bdc7dc1cf5ece54bd8572786419bc1c7fb
unzelfunzel <unzel@byom.de>
2020-11-08 19:27:19 +0100

    update

--- The Beer Game/The Beer Game/Participant.cs
+++ The Beer Game/The Beer Game/Participant.cs
@@ -18,6 +18,46 @@ namespace The_Beer_Game
             Bank = money;
         }
         
+        // Method to show inventory:
+        int get_invetory()
+        {
+            return Invetory;
+        }
+        
+        // Method to show bank:
+        void get_bank()
+        {
+            return Bank;
+        }
+        
+        // Method to receive order:
+        void receive_order(int amount, int price)
+        {
+            
+        }
+        
+        // Method to place order:
+        void place_order(int amount, int price)
+        {
+            
+        }
+        
+        // Method to process incoming goods:
+        void incoming_goods(int amount)
+        {
+            
+        }
+        
+        // Method to process outgoing goods:
+        void outgoing_goods(int amount)
+        {
+            
+        }
+        
+        // deconstructor:
+        ~Participant()
+        {
+            // tbd
+        }
     }
-
 }
